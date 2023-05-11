930291bf35202a687cfa78c121776b718493b2a4
yaminig13 <helloyamini@gmail.com>
2023-05-11 15:04:28 +0200

    - Deletion of entry is not possible from backend - Addition of gynaec view and data

---
+++ pages/Gynaec.vue
@@ -0,0 +1,132 @@
+<template>
+  <main>
+    <Topbar></Topbar>
+    <Header :src="gynaec.image" :text="gynaec.title" buttons="false" link="/gynaec"></Header>
+    <div class="ortho">
+      <!-- <div class="ortho__header--text">
+        <span>
+          To book appointment, call us at
+        </span>
+        <span>
+          <i class="fa-solid fa-phone-flip"></i>
+          123456
+        </span>
+        <span>
+          <i class="fa-solid fa-phone-flip"></i>
+          123456
+        </span>
+        <span>
+          <i class="fa-solid fa-phone-flip"></i>
+          123456
+        </span>
+      </div> -->
+
+      <div class="ortho__highlights service">
+
+        <div class="ortho__highlights--title service"> Services </div>
+
+        <div class="ortho__highlights--wrapper service">
+          <div class="ortho__highlights--item service" v-for="service in gynaec.services">
+            <i class="fa-solid fa-x-ray"></i>
+            <span> {{ service }} </span>
+          </div>
+        </div>
+      </div>
+
+      <div class="ortho__highlights surgery">
+        <div class="ortho__highlights--title surgery"> Surgeries </div>
+
+        <div class="ortho__highlights--wrapper surgery">
+          <div class="ortho__highlights--item surgery" v-for="surgery in gynaec.surgeries">
+            <span> {{ surgery }} </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+   async asyncData({ $content }) {
+    const gynaecs = await $content("gynaec").fetch();
+    const gynaec=gynaecs[0]; 
+    return {
+      gynaec,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+
+.ortho {
+  &__header--text {
+    position: absolute;
+    top: -10rem;
+    width: 50%;
+    text-align: center;
+    color: white;
+    display: flex;
+    flex-direction: column;
+  }
+
+  &__highlights {
+    display: flex;
+    padding: 2rem;
+    justify-content: space-between;
+    flex-direction: column;
+    
+    &.service {
+      background-color: #093824;
+    }
+
+    &--info {
+      font-size: x-small;
+    }
+
+    &--title {
+      text-align: center;
+      font-size: xx-large;
+
+      &.service {
+        color: #f5f9e9;
+      }
+    }
+
+
+    &--wrapper {
+      display: flex;
+      justify-content: center;
+    }
+
+    &--item {
+      display: flex;
+      flex-direction: column;
+      border: 2px solid black;
+      margin: 2rem;
+      padding: 2rem;
+      justify-content: center;
+      align-items: center;
+
+      &.service {
+        background-color: #f5f9e9;
+        color: #093824;
+        height: 10rem;
+        width: 15rem;
+      }
+
+      &.surgery {
+        background-color: #093824;
+        color: #f5f9e9;
+        height: 5rem;
+        width: 10rem;
+      }
+      svg {
+        font-size: xx-large;
+        padding-bottom: 1rem;
+      }
+    }
+  }
+}
+</style>
